47311fdb8f4a7720b0203aceaf9848b0d167197d
Dmytro Medvediev <s_medvedev@outlook.com>
2023-03-01 19:35:52 +0100

    update slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,16 +1,16 @@
-// $('.slider').slick({
-//     arrows: true,
-//     dots: false,
-//     infinite: true,
-//     slidesToShow: 1,
-//     slidesToScroll: 1
-// });
-  
-  $('.autoplay').slick({
-    dots: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    dotsClass: "my-dots",
-    autoplay: true,
-    autoplaySpeed: 2000,
+$('.slider').slick({
+    arrows: true,
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
 });
+  
+//   $('.autoplay').slick({
+//     dots: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     dotsClass: "my-dots",
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+// });
