c2796a27864cafb635bf30b4046b095c18080efb
Dmytro Medvediev <s_medvedev@outlook.com>
2023-03-01 19:39:47 +0100

    update slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,16 +1,14 @@
+// $('.slider').slick({
+//     arrows: true,
+//     dots: false,
+//     infinite: true,
+//     slidesToShow: 1,
+//     slidesToScroll: 1
+// });
+  
 $('.slider').slick({
     arrows: true,
     dots: false,
-    infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1
 });
-  
-//   $('.autoplay').slick({
-//     dots: true,
-//     slidesToShow: 3,
-//     slidesToScroll: 1,
-//     dotsClass: "my-dots",
-//     autoplay: true,
-//     autoplaySpeed: 2000,
-// });
